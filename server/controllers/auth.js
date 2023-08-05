0e595be17e179907c0f88ec87fe5d32f8a3030dd
Nidhish Chauhan <84083897+Nidhish-07@users.noreply.github.com>
2023-08-05 08:57:30 +0000

    callback changed to async/await

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,27 +1,32 @@
-import User from "../models/User.js"
+import User from "../models/User.js";
 
-export const signup = (req, res) => {
+export const signup = async (req, res) => {
   const { name, email, password } = req.body;
 
-  User.findOne({ email }).exec((err, user) => {
-    if (user) {
-      return res.status(400).json({
-        error: "Email already exists",
-      });
-    }
-  });
+  const user = await User.findOne({ email: email }).exec();
+
+  if (user) {
+    return res.status(400).json({
+      error: "Email already exists",
+    });
+  }
+
+  // User.findOne({ email: email })
+  //   .then((user) => {
+  //     if (user) {
+  //       return res.status(400).json({
+  //         error: "Email already exists",
+  //       });
+  //     }
+  //   })
+  //   .catch((err) => res.status(400).json({ error: err }));
 
   let newUser = new User({ name, email, password });
 
-  newUser.save((err, success) => {
-    if (err) {
-      console.log("Signup error", err);
-      return res.status(400).json({ error: err });
-    }
-  });
+  const savedUser = await newUser.save();
 
-  return res.json({
+  res.json({
     message: "Signup Success",
-    user: newUser,
+    user: savedUser,
   });
 };
